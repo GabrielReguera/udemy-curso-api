13966104a2df05ed16cde4c7fd95ce3cc5702080
GabrielReguera <gabrielreguera121@hotmail.com>
2022-10-20 15:48:01 -0300

    First commit

---
+++ src/main/java/com/helpdesk/udemycursoapi/UdemyCursoApiApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesk.udemycursoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCursoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyCursoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpdesk/udemycursoapi/UdemyCursoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk.udemycursoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCursoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
