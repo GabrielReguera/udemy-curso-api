cf94b9f29fda14bb6064a4db652b912c3308fa48
GabrielReguera <gabrielreguera121@hotmail.com>
2022-10-21 15:46:27 -0300

    add repositories

--- src/test/java/com/valdir/helpdesk/HelpdeskApplicationTests.java
+++ src/test/java/com/valdir/helpdesk/HelpdeskApplicationTests.java
@@ -1,10 +1,10 @@
-package com.helpdesk.udemycursoapi;
+package com.valdir.helpdesk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UdemyCursoApiApplicationTests {
+class HelpdeskApplicationTests {
 
 	@Test
 	void contextLoads() {
