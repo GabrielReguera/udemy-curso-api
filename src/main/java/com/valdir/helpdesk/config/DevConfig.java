394ccf59597c657e1a2405dde3dc6fa465e24f63
GabrielReguera <gabrielreguera121@hotmail.com>
2022-10-24 09:50:37 -0300

    Config dev/test profile

---
+++ src/main/java/com/valdir/helpdesk/config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.valdir.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.valdir.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    public boolean instaciaDB() {
+        if (value.equals("update")) {
+            dbService.instanciaDB();
+        }
+        return false;
+    }
+}
