1a9da780cffb54fbbb8365fc295ace45af677791
GabrielReguera <gabrielreguera121@hotmail.com>
2022-11-01 11:52:11 -0300

    test5

--- src/main/java/com/valdir/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/valdir/helpdesk/resources/ChamadoResource.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +21,6 @@ import com.valdir.helpdesk.domain.Chamado;
 import com.valdir.helpdesk.domain.dtos.ChamadoDto;
 import com.valdir.helpdesk.services.ChamadoService;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping(value = "/chamados")
 public class ChamadoResource {
--- src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
@@ -9,7 +9,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +23,6 @@ import com.valdir.helpdesk.domain.Tecnico;
 import com.valdir.helpdesk.domain.dtos.TecnicoDto;
 import com.valdir.helpdesk.services.TecnicoService;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
